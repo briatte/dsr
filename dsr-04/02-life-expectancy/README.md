dbf0a0af627af5e114e5101613a80732967a163f
François Briatte <briatte@gmail.com>
2023-02-15 22:15:59 +0100

    Session 4: life expectancy mapping example

---
+++ dsr-04/02-life-expectancy/README.md
@@ -0,0 +1,17 @@
+# README
+
+__Data source for shapefiles:__ [World Bank Official Boundaries][owb].
+
+[owb]: https://datacatalog.worldbank.org/search/dataset/0038272/World-Bank-Official-Boundaries
+
+__Data source for life expectancy:__ World Bank [World Development Indicators][wdi].
+
+[wdi]: https://databank.worldbank.org/source/world-development-indicators
+
+The latter were retrieved with the `WDI` package:
+
+```r
+library(WDI)
+WDI::WDI(country = "all", indicator = "SP.DYN.LE00.IN", latest = 1) %>% 
+  write_tsv("data/life-expectancy.tsv")
+```
---
+++ dsr-04/02-life-expectancy/life-expectancy.r
@@ -0,0 +1,90 @@
+# ------------------------------------------------------------------------------
+# Simple choropleth maps, using World Bank data + shapefiles.
+# ------------------------------------------------------------------------------
+
+library(sf)
+library(tidyverse)
+
+# ------------------------------------------------------------------------------
+# Step 1: import the shapefile
+# ------------------------------------------------------------------------------
+
+geo <- "data/WB_countries_Admin0_lowres.geojson"
+geo <- sf::st_read(geo)
+
+# ------------------------------------------------------------------------------
+# Step 2: CRS projections and bounding boxes
+# ------------------------------------------------------------------------------
+
+# the data look like a data frame, but contain spatial geometries
+glimpse(geo)
+
+# world map, with land and country administrative borders
+ggplot(geo) +
+  geom_sf() +
+  coord_sf(crs = 4326)
+
+# the previous plot uses the standard WGS84 CRS projection
+sf::st_crs("EPSG:4326")
+
+# Denmark -- finding the 'bounding box' (min/max lat/lon coordinates)
+#
+filter(geo, NAME_EN == "Denmark") %>% 
+  sf::st_bbox()
+
+# Denmark -- manually defining the 'bounding box'
+#
+ggplot(filter(geo, NAME_EN == "Denmark")) +
+  geom_sf() +
+  coord_sf(ylim = c(54.6, 57.8), # y = latitude = horizontal
+           xlim = c(8.1, 15.2))  # x = longitude = vertical
+
+# U.S. -- default projection
+#
+ggplot(filter(geo, NAME_EN == "United States of America")) +
+  geom_sf()
+
+# U.S. -- Arctic Polar Stereographic projection
+#
+ggplot(filter(geo, NAME_EN == "United States of America") %>% 
+         sf::st_transform(crs = "EPSG:3995")) +
+  geom_sf()
+
+# ------------------------------------------------------------------------------
+# Step 3: import life expectancy
+# ------------------------------------------------------------------------------
+
+lexp <- readr::read_tsv("data/life-expectancy.tsv")
+glimpse(lexp)
+
+# sanity check: only one measure per country
+stopifnot(!duplicated(lexp$country))
+
+# merge to geographic data (after exploring the country codes a bit...)
+d <- full_join(geo, lexp, by = c("WB_A3" = "iso3c"))
+
+# ------------------------------------------------------------------------------
+# Step 4: project onto choropleth map
+# ------------------------------------------------------------------------------
+
+# fiddling with many plot parameters to show how scales and themes work
+ggplot(d, aes(fill = SP.DYN.LE00.IN)) +
+  geom_sf() +
+  coord_sf(crs = 4326) +
+  # legend color, title and shape
+  scale_fill_viridis_c("Years", guide = guide_legend()) +
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "#b3cee5"), # pale light blue
+    legend.position = "bottom",
+    legend.key.width = unit(1.5, "cm"),
+    plot.title = element_text(face = "bold"),
+    plot.margin = margin(1 ,1 , 1, 1, "cm")
+  ) +
+  labs(title = "Life expectancy at birth",
+       subtitle = "Data: World Development Indicators, 2020")
+
+# export with specific dimensions
+ggsave("life-expectancy.png", width = 8, height = 4.5)
+
+# kthxbye
