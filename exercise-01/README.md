7311b3fe53f11bf5f8fbab5ae5309d25e13c5d71
François Briatte <briatte@gmail.com>
2023-02-16 05:06:51 +0100

    Exercise 1

---
+++ exercise-01/README.md
@@ -0,0 +1,9 @@
+# README
+
+__Source:__ [Antonio Sánchez Chinchón][asc], on Twitter: [example 1][asc1], [example 2][asc2].
+
+[asc]: https://fronkonstin.com/
+[asc1]: https://twitter.com/aschinchon/status/1614707026415820800
+[asc2]: https://twitter.com/aschinchon/status/1615090579528118272
+
+Relevant Twitter hashtags to find more examples: [`#rstats` `#generativeart` `#ggplot2`](https://twitter.com/search?q=%23rstats%20%23generativeart%20%23ggplot2&src=typed_query).
--- exercise-01/generative-art.r
+++ exercise-01/generative-art.r
@@ -12,7 +12,7 @@ library(tidyverse)
 
 seq(from = -5, to = 10, by = 0.05) %>%
   expand.grid(x = ., y = .) %>%
-  ggplot(aes(x = (x + pi*sin(y)), y = (y + pi*sin(x)))) +
+  ggplot(aes(x = (x + pi * sin(y)), y = (y + pi * sin(x)))) +
   geom_point(alpha = .1, shape = 20, size = 1, color = "darkred")+
   coord_polar() +
   theme_void()
@@ -21,9 +21,9 @@ seq(from = -5, to = 10, by = 0.05) %>%
 # https://twitter.com/aschinchon/status/1615090579528118272
 
 seq(1, 200, .0005) %>%
-  tibble(x = exp(-0.006*.) * sin(. * 3.01 + 1.68) + sin(. *2.96 + 2.72),
-         y = exp(-0.009*.) * sin(. * 3.03 + 1.23) + sin(. *2.98 + 1.28)) %>%
-  ggplot(aes(x,y)) +
+  tibble(x = exp(-0.006 * .) * sin(. * 3.01 + 1.68) + sin(. * 2.96 + 2.72),
+         y = exp(-0.009 * .) * sin(. * 3.03 + 1.23) + sin(. * 2.98 + 1.28)) %>%
+  ggplot(aes(x, y)) +
   geom_path(linewidth = 1/3, color = "steelblue")+
   coord_equal() +
   theme_void()
