e15e681c3c34198f8f023eb2467cf624ad4b940b
François Briatte <briatte@gmail.com>
2024-09-27 23:22:04 +0200

    Update exercise-05.r

--- exercise-05/exercise-05.r
+++ exercise-05/exercise-05.r
@@ -12,20 +12,6 @@
 # Student 3:
 # Date:
 #
-# Answers to questions
-# --------------------
-#
-# [ANSWER 1.1]:
-# [ANSWER 1.2]:
-# [ANSWER 2]:
-# [ANSWER 3]:
-# [ANSWER 4]:
-# [ANSWER 6.1]:
-# [ANSWER 6.2]:
-# [ANSWER 7.1]:
-# [ANSWER 7.2]:
-# [ANSWER 8]:
-#
 # Feedback on the exercise
 # ------------------------
 #
@@ -45,18 +31,11 @@ library(tidyverse)
 
 
 
-# [QUESTION 1.1]: Is this a longitudinal dataset?
-#                 Answer TRUE or FALSE.
-# [QUESTION 1.2]: Is this a cross-sectional dataset?
-#                 Answer TRUE or FALSE.
-
 # ------------------------------------------------------------------------------
 # 2. Subset and count observations
 # ------------------------------------------------------------------------------
 
 
-
-# [QUESTION 2]: What is the sample size of your study?
 
 # ------------------------------------------------------------------------------
 # 3. Create a dummy variable
@@ -64,16 +43,11 @@ library(tidyverse)
 
 
 
-# [QUESTION 3] Are there more females than males in your sample?
-#              Answer TRUE or FALSE.
-
 # ------------------------------------------------------------------------------
 # 4. Recode missing values
 # ------------------------------------------------------------------------------
 
 
-
-# [QUESTION 4] For how many respondents is age missing in your sample?
 
 # ------------------------------------------------------------------------------
 # 5. Plot a distribution
@@ -87,22 +61,11 @@ library(tidyverse)
 
 
 
-# [QUESTION 6.1] In your sample, is the average age above the median age?
-# [QUESTION 6.2] What is the age of the 80th percentile of your sample?
-
 # ------------------------------------------------------------------------------
-# 7. Explore some relationships
+# 7. Recode variables
 # ------------------------------------------------------------------------------
 
 
-
-# [QUESTION 7.1] In your sample, is the highest age group the one that reports
-#                the highest level of trust in Islamist parties?
-#                Answer TRUE or FALSE.
-#
-# [QUESTION 7.2] In your sample, to what extent do females trust Islamist
-#                parties more (or less) than males?
-#                Answer with a percentage point difference, rounded to 1 digit.
 
 # ------------------------------------------------------------------------------
 # 8. Test a relationship
@@ -110,8 +73,10 @@ library(tidyverse)
 
 
 
-# [QUESTION 8] In your sample, do females statistically differ from males in
-#              their level of trust in Islamist parties?
-#              Answer TRUE or FALSE.
+# ------------------------------------------------------------------------------
+# 9. Report percentages
+# ------------------------------------------------------------------------------
+
+
 
 # You are done -- thank you for your efforts!
